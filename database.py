9b7ccc11093fa37552e1146975a011e8926a8d7e
Mahajurul Karim <inversemaha@users.noreply.github.com>
2025-11-28 02:30:29 +0600

    Update database.py

--- database.py
+++ database.py
@@ -3,6 +3,3 @@ from pymongo import MongoClient
 def database():
     client = MongoClient('mongodb://localhost:27017/')
     return client['pharmacy_scraper']
-
-    # client = MongoClient('mongodb+srv://username:password@scrap.czefu97.mongodb.net/development')
-    # return client['development']
